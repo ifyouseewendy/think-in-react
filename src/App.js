f663544f5f22fdfba0d6605ba3063b5d4b0fe1a6
Di Wen <ifyouseewendy@gmail.com>
2017-04-14 13:03:15 -0400

    Display products

--- src/App.js
+++ src/App.js
@@ -4,15 +4,54 @@ import React, {
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+      inputChecked: false
+    };
+  }
+
+  displayProducts() {
+    const products = this.props.payload;
+    let rows = [];
+
+    products.forEach((product, index) => {
+      if(index == 0 || product.category != products[index-1].category) {
+        rows.push(
+          <th>
+            <td colSpan='2'>{product.category}</td>
+          </th>
+        );
+      }
+      rows.push(
+        <tr>
+          <td className={product.stocked ? '' : 'out-of-stock'}>{product.name}</td>
+          <td>{product.price}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <tbody>
+      {rows}
+      </tbody>
+    );
+  }
+
   render() {
     return (
       <div className='product'>
+        <div className='debug'>
+          {this.props.payload[0].category}
+        </div>
         <div className='search'>
           <div className='search-input'>
-            <input type='text' placeholder='Search...' />
+            <input type='text' placeholder='Search...' value={this.state.inputValue} />
           </div>
           <div className='search-filter'>
-            <input type='checkbox' /><span>Only show products in stock</span>
+            <input type='checkbox' checked={this.state.inputChecked} /><span>Only show products in stock</span>
           </div>
         </div>
         <div className='Display'>
@@ -23,15 +62,7 @@ class App extends Component {
                 <td>Price</td>
               </tr>
             </thead>
-            <tbody>
-              <th>
-                <td colSpan='2'>Sporting Goods</td>
-              </th>
-              <tr>
-                <td>Football</td>
-                <td>$49.99</td>
-              </tr>
-            </tbody>
+            {this.displayProducts()}
           </table>
         </div>
       </div>
