bf759791bc512704b4519dc0bef54ea1a8a6c3d7
Di Wen <ifyouseewendy@gmail.com>
2017-04-14 13:25:22 -0400

    Interact on input change

--- src/App.js
+++ src/App.js
@@ -11,14 +11,39 @@ class App extends Component {
       inputValue: '',
       inputChecked: false
     };
+
+    this.inputChanged = this.inputChanged.bind(this);
+    this.inputChecked = this.inputChecked.bind(this);
   }
 
-  displayProducts() {
-    const products = this.props.payload;
-    let rows = [];
+  inputChanged(e) {
+    const value = e.target.value;
+    this.setState({inputValue: value});
+  }
 
+  inputChecked(e) {
+    const value = e.target.checked;
+    this.setState({inputChecked: value});
+  }
+
+  displayProducts(payload) {
+    const value = this.state.inputValue;
+    const inStock = this.state.inputChecked;
+
+    const products = payload.slice().filter((product) => {
+      if (inStock && !product.stocked) {
+        return false;
+      }
+      if (value && product.name.indexOf(value) === -1) {
+        return false;
+      }
+
+      return true;
+    });
+
+    let rows = [];
     products.forEach((product, index) => {
-      if(index == 0 || product.category != products[index-1].category) {
+      if (index == 0 || product.category != products[index-1].category) {
         rows.push(
           <th>
             <td colSpan='2'>{product.category}</td>
@@ -44,14 +69,16 @@ class App extends Component {
     return (
       <div className='product'>
         <div className='debug'>
-          {this.props.payload[0].category}
+          <p>inputValue: {this.state.inputValue}</p>
+          <p>inputChecked: {this.state.inputChecked ? 'true' : 'false'}</p>
         </div>
         <div className='search'>
           <div className='search-input'>
-            <input type='text' placeholder='Search...' value={this.state.inputValue} />
+            <input type='text' placeholder='Search...' value={this.state.inputValue} onChange={this.inputChanged} />
           </div>
           <div className='search-filter'>
-            <input type='checkbox' checked={this.state.inputChecked} /><span>Only show products in stock</span>
+            <input type='checkbox' checked={this.state.inputChecked} onChange={this.inputChecked} />
+            <span>Only show products in stock</span>
           </div>
         </div>
         <div className='Display'>
@@ -62,7 +89,7 @@ class App extends Component {
                 <td>Price</td>
               </tr>
             </thead>
-            {this.displayProducts()}
+            {this.displayProducts(this.props.payload)}
           </table>
         </div>
       </div>
